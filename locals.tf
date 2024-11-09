00cc845c530b9e55e1cf77a3e892a78efd2b41bb
christos-tsatsoulas <christos@fastmail.com>
2024-11-09 11:40:30 +0000

    Correct Enviroment

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
-  env         = "staging"
-  region      = "us-east-2"
-  zone1       = "us-east-2a"
-  zone2       = "us-east-2b"
+  env         = "production"
+  region      = "eu-west-1"
+  zone1       = "eu-west-1a"
+  zone2       = "eu-west-1b"
   eks_name    = "demo"
   eks_version = "1.30"
 }
